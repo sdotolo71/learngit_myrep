5f576478149ee75fcd84939e42d3f3bc011db891
sdotolo71 <dotolo.salvatore@gmail.com>
2019-09-14 16:52:19 +0200

    base spring boot project (test only) - first commit ever!

---
+++ src/main/java/com/example/base/LearngitMyrepApplication.java
@@ -0,0 +1,13 @@
+package com.example.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearngitMyrepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearngitMyrepApplication.class, args);
+	}
+
+}
